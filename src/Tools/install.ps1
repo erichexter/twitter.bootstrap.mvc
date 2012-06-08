04917d8ff4b7a3340a9b6b237b1b2fc79bf749f5
Eric Hexter <eric.hexter@gmail.com>
2012-06-08 15:00:56 -0500

    added install script to register the bundle config

---
+++ src/Tools/install.ps1
@@ -0,0 +1,41 @@
+param($installPath, $toolsPath, $package, $project)
+
+
+$proj = $project
+
+$globalasax = $null
+try
+{
+    $globalasax = $proj.ProjectItems.Item("global.asax")
+}
+catch {
+    
+}
+
+if($globalasax -ne $null) {
+    try{
+    	$mvcapp = $globalasax.ProjectItems.Item("global.asax.cs").FileCodeModel.CodeElements | where-object {$_.Kind -eq 5}
+
+    			$startup = $mvcapp.Children.Item(1).Members | Where-object {$_.Name -eq 'Application_Start'}
+    			if($startup -ne $null) 
+    			{	
+				$editpoint=$startup.EndPoint.CreateEditPoint()
+				$editpoint.LineUp(1)
+				$editpoint.EndOfLine()
+				$editpoint.InsertNewLine()
+				$editpoint.Insert("BootstrapBundleConfig.RegisterBundles(System.Web.Optimization.BundleTable.Bundles);
+
+")						    						}
+    }
+    catch{
+        "Error adding bundle config call to the Application_Start"|out-default
+        $_.Exception.ToString()| out-default
+   }
+}
+
+
+
+
+
+
+
--- src/readme.txt
+++ src/readme.txt
@@ -1,8 +1,15 @@
 Easy bootstrap for your MVC4 application
 
+http://twitter.github.com/bootstrap/
+
+
+
+
 
 
 Add this to your global.asax.cs Application_Start method
+
+
 
 
 BootstrapBundleConfig.RegisterBundles(System.Web.Optimization.BundleTable.Bundles);
