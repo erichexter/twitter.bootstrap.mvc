5abdf258da5c4042b37b565dd536d86dfba66c49
Eric Hexter <eric.hexter@gmail.com>
2012-09-07 20:05:47 -0500

    updated to release version

---
+++ src/Controllers/BootstrapBaseController.cs.pp
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using $rootnamespace$.Models;
+
+namespace $rootnamespace$.Controllers
+{
+    public class BootstrapBaseController: Controller
+    {
+
+	public void Success(string message)
+	{
+                TempData.Add("success",message);	
+	}
+	public void Information(string message){
+                TempData.Add("info",message);		
+	}
+	public void Error(string message){
+        	TempData.Add("error",message);				
+	}
+    }
+}
