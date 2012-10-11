74a40da4bdfbcba442a276e0e6a49d2ed5d1cd32
Eric Hexter <eric.hexter@gmail.com>
2012-10-11 16:12:32 -0500

    Added default index scaffolding

---
+++ src/BootstrapSupport/ViewHelperExtensions.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace BootstrapSupport
+{
+    public static class DefaultScaffoldingExtensions
+    {
+        public static string GetControllerName(this Type controllerType)
+        {
+            return controllerType.Name.Replace("Controller", String.Empty);
+        }
+
+        public static string GetActionName(this LambdaExpression actionExpression)
+        {
+            return ((MethodCallExpression)actionExpression.Body).Method.Name;
+        }
+
+
+        public static PropertyInfo[] VisibleProperties(this HtmlHelper helper, IEnumerable Model)
+        {
+            var elementType = Model.GetType().GetElementType();
+            if (elementType == null)
+            {
+                elementType = Model.GetType().GetGenericArguments()[0];
+            }
+            return elementType.GetProperties().Where(info => info.PropertyType != typeof(Guid)).ToArray();
+        }
+
+        public static PropertyInfo[] VisibleProperties(this Object model)
+        {
+            return model.GetType().GetProperties().Where(info => info.PropertyType != typeof(Guid)).ToArray();
+        }
+
+        public static RouteValueDictionary GetIdValue(this object model)
+        {
+            var v = new RouteValueDictionary();
+            v.Add(model.IdentifierPropertyName(), model.GetId());
+            return v;
+        }
+        public static object GetId(this object model)
+        {
+            return model.GetType().GetProperty(model.IdentifierPropertyName()).GetValue(model,new object[0]);
+        }
+        public static string IdentifierPropertyName(this Object model)
+        {
+            if(model.GetType().GetProperties().Any(info => info.PropertyType.AttributeExists<System.ComponentModel.DataAnnotations.KeyAttribute>()))
+            {
+                return
+                    model.GetType().GetProperties().First(
+                        info => info.PropertyType.AttributeExists<System.ComponentModel.DataAnnotations.KeyAttribute>())
+                        .Name;
+            }
+            else if(model.GetType().GetProperties().Any(p=>p.Name.Equals("id",StringComparison.CurrentCultureIgnoreCase)))
+            {
+                return
+                    model.GetType().GetProperties().First(
+                        p => p.Name.Equals("id", StringComparison.CurrentCultureIgnoreCase)).Name;
+            }
+            return "";
+        }
+
+        public static string GetLabel(this PropertyInfo propertyInfo)
+        {
+            var meta = ModelMetadataProviders.Current.GetMetadataForProperty(null, propertyInfo.DeclaringType, propertyInfo.Name);
+            return meta.GetDisplayName();
+        }
+        public static string ToSeparatedWords(this string value)
+        {
+            return Regex.Replace(value, "([A-Z][a-z])", " $1").Trim();
+        }
+
+    }
+    public static class PropertyInfoExtensions
+    {
+        public static bool AttributeExists<T>(this PropertyInfo propertyInfo) where T : class
+        {
+            var attribute = propertyInfo.GetCustomAttributes(typeof(T), false)
+                                .FirstOrDefault() as T;
+            if (attribute == null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static bool AttributeExists<T>(this Type type) where T : class
+        {
+            var attribute = type.GetCustomAttributes(typeof(T), false).FirstOrDefault() as T;
+            if (attribute == null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static T GetAttribute<T>(this Type type) where T : class
+        {
+            return type.GetCustomAttributes(typeof(T), false).FirstOrDefault() as T;
+        }
+
+        public static T GetAttribute<T>(this PropertyInfo propertyInfo) where T : class
+        {
+            return propertyInfo.GetCustomAttributes(typeof(T), false).FirstOrDefault() as T;
+        }
+    }
+}
