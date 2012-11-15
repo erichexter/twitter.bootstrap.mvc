6585b6306fa857d77f62313cc160453d8144b526
Mike Paterson <michael.l.paterson@gmail.com>
2012-11-15 08:17:40 -0500

    Added using statement in ViewHelperExtensions
    Added missing using statement `System.Web.Mvc.Html`

--- src/BootstrapSupport/ViewHelperExtensions.cs
+++ src/BootstrapSupport/ViewHelperExtensions.cs
@@ -5,6 +5,7 @@ using System.Linq.Expressions;
 using System.Reflection;
 using System.Text.RegularExpressions;
 using System.Web.Mvc;
+using System.Web.Mvc.Html;
 using System.Web.Routing;
 
 namespace BootstrapSupport
