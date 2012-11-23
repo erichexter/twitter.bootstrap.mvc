3a11482b6942a994173407d4752226994624e1db
Eric Hexter <eric.hexter@gmail.com>
2012-11-23 15:47:51 -0600

    added project and tests for NavigationRoutes.
    This current implementation has a depenedency on Mvc3Futures.
    to use this install mvc3Futures -IgnoreDependencies. the aspnet team
    will release a mvc4Futures the week of 11/26/2012

---
+++ src/NavigationRoutes/UnitTests/Navigation_Route_Should.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Should;
+using NUnit.Framework;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web;
+using System.IO;
+using NavigationRoutes;
+namespace UnitTests
+{
+    [TestFixture]
+    public class Navigation_Route_Should:RouteTesterBase
+    {
+        [Test]
+        public void Map_a_route_to_a_url_matching_the_action_name()
+        {
+            var routes = new System.Web.Routing.RouteCollection();
+
+            routes.MapNavigationRoute<HomeController>("Home", c => c.Index());
+            routes.MapNavigationRoute<HomeController>("About", c => c.About());
+
+
+            routes.Count.ShouldNotEqual(0);
+
+            var uh = GetUrlHelper(routes);
+            uh.RouteUrl("Navigation-Home-Index").ShouldEqual("/index");
+            uh.RouteUrl("Navigation-Home-About").ShouldEqual("/about");
+ 
+        }
+    }
+}
---
+++ src/NavigationRoutes/UnitTests/RouteTesterBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace UnitTests
+{
+    public class RouteTesterBase
+    {
+        public UrlHelper GetUrlHelper( RouteCollection routeCollection,string fileName = "/",string url = "http://localhost",string queryString = "")
+        {
+            //Make a request context
+            var request = new HttpRequest(fileName, url, queryString);
+            var response = new HttpResponse(new StringWriter());
+            var httpContext = new HttpContext(request, response);
+            var httpContextBase = new HttpContextWrapper(httpContext);
+
+            // Make the UrlHelper with empty route data
+            var requestContext = new RequestContext(httpContextBase, new RouteData());
+            return new UrlHelper(requestContext, routeCollection);
+        }
+
+    }
+}
