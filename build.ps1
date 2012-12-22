c2a38940298a0941ac8ea084a247e286dda1610f
Eric Hexter <eric.hexter@gmail.com>
2012-12-21 23:55:06 -0600

    fixed nuget specs

--- build.ps1
+++ build.ps1
@@ -1,10 +1,10 @@
 Param($path = "output")
 
 New-Item -ItemType d -Force $path -ErrorAction SilentlyContinue
-.nuget\nuget.exe pack src\NavigationRoutes\navigation-routes-mvc4.nuspec -o $path
-.nuget\nuget.exe pack src\Bootstrap\twitter-bootstrap-mvc.nuspec -o $path
-.nuget\nuget.exe pack src\Bootstrap\twitter-bootstrap-mvc-sample.nuspec -o $path
-.nuget\nuget.exe pack src\Bootstrap\twitter-bootstrap-mvc-templates.nuspec -o $path
+.nuget\nuget.exe pack src\navigation-routes-mvc4.nuspec -o $path
+.nuget\nuget.exe pack src\twitter-bootstrap-mvc.nuspec -o $path
+.nuget\nuget.exe pack src\twitter-bootstrap-mvc-sample.nuspec -o $path
+.nuget\nuget.exe pack src\twitter-bootstrap-mvc-templates.nuspec -o $path
 $o= resolve-path $path
 
 "To test these packages add a nuget source (in visual studio) to:"
