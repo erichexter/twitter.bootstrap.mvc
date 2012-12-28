ab3d1e451a261f0a72b82d5c0fd22cb0906b9a34
Marijn van der Zee <marijn@serraict.com>
2012-12-28 20:44:01 +0100

    Add html helpers nuspec file

--- build.ps1
+++ build.ps1
@@ -5,6 +5,7 @@ New-Item -ItemType d -Force $path -ErrorAction SilentlyContinue
 .nuget\nuget.exe pack src\twitter-bootstrap-mvc.nuspec -o $path
 .nuget\nuget.exe pack src\twitter-bootstrap-mvc-sample.nuspec -o $path
 .nuget\nuget.exe pack src\twitter-bootstrap-mvc-templates.nuspec -o $path
+.nuget\nuget.exe pack src\twitter-bootstrap-mvc-htmlhelpers.nuspec -o $path
 $o= resolve-path $path
 
 "To test these packages add a nuget source (in visual studio) to:"
