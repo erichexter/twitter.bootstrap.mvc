c6d26edec09f4298dbb74c8a055ffabac841023d
James Chambers <james@jameschambers.com>
2013-01-30 11:22:06 -0500

    Support for Vertical/Horizontal Breaks in Navbar
    Added property to NamedRoute to allow composition of break in menu.
    Added overloads to accept the flag in the configuration extensions.
    Updated navigation extenstions to evaluate and build appropriate tags.
    Updated samples.

--- src/NavigationRoutes/NavigationRoutes/NavigationRouteConfigurationExtensions.cs
+++ src/NavigationRoutes/NavigationRoutes/NavigationRouteConfigurationExtensions.cs
@@ -47,21 +47,23 @@ namespace NavigationRoutes
         }
 
 
-        public static NavigationRouteBuilder MapNavigationRoute<T>(this RouteCollection routes, string displayName, Expression<Func<T, ActionResult>> action) where T : IController
+        public static NavigationRouteBuilder MapNavigationRoute<T>(this RouteCollection routes, string displayName, Expression<Func<T, ActionResult>> action, bool breakAfter = false) where T : IController
         {
             var newRoute = new NamedRoute("", "", new MvcRouteHandler());
             newRoute.ToDefaultAction(action);
             newRoute.DisplayName = displayName;
+            newRoute.ShouldBreakAfter = breakAfter;
             routes.Add(newRoute.Name, newRoute);
             return new NavigationRouteBuilder(routes, newRoute);
         }
 
-        public static NavigationRouteBuilder AddChildRoute<T>(this NavigationRouteBuilder builder, string DisplayText, Expression<Func<T, ActionResult>> action) where T : IController
+        public static NavigationRouteBuilder AddChildRoute<T>(this NavigationRouteBuilder builder, string DisplayText, Expression<Func<T, ActionResult>> action, bool breakAfter = false) where T : IController
         {
             var childRoute = new NamedRoute("", "", new MvcRouteHandler());
             childRoute.ToDefaultAction<T>(action);
             childRoute.DisplayName = DisplayText;
             childRoute.IsChild = true;
+            childRoute.ShouldBreakAfter = breakAfter;
             builder._parent.Children.Add(childRoute);
             builder._routes.Add(childRoute.Name,childRoute);
             return builder;
