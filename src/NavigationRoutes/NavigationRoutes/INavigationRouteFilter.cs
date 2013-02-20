d99b6baa1007beb82653722f0ef90dd0d42ba537
Eric Hexter <eric.hexter@gmail.com>
2013-02-20 13:45:07 -0600

    removed navigation routes

--- src/NavigationRoutes/NavigationRoutes/INavigationRouteFilter.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Web.Routing;
-namespace NavigationRoutes
-{
-    public interface INavigationRouteFilter
-    {
-        bool  ShouldRemove(Route navigationRoutes);
-    }
-}
