05255fa4604485b6c947f854a7d703d0915cd453
Mahmoud Ali <muddibr@gmail.com>
2013-08-14 22:03:26 -0400

    Scripts folder reference normalization
    Fix the Scripts folder path so it's standarized and doesn't get included
    twice when used in MVC 4 project with a Bundle.

--- src/Bootstrap/App_Start/BootstrapBundleConfig.cs
+++ src/Bootstrap/App_Start/BootstrapBundleConfig.cs
@@ -13,7 +13,7 @@ namespace BootstrapSupport
                 "~/Scripts/jquery-migrate-{version}.js",
                 "~/Scripts/bootstrap.js",
                 "~/Scripts/jquery.validate.js",
-                "~/scripts/jquery.validate.unobtrusive.js",
+                "~/Scripts/jquery.validate.unobtrusive.js",
                 "~/Scripts/jquery.validate.unobtrusive-custom-for-bootstrap.js"
                 ));
 
